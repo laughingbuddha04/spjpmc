c65e6c7f84d1d9353ad274419c28cdc1fc23bae4
laughingbuddha04 <78607000+laughingbuddha04@users.noreply.github.com>
2021-02-07 10:01:20 +0000

    Update App.test.tsx

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,20 +1,21 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+describe('spjpmc test suit', () => {
+  test('renders headers', () => {
+    render(<App />);
+    const header = screen.getByText(/Stock Market/i);
+    const transactions = screen.getByText(/Transaction/i);
+    const trade = screen.getByText(/Trade/i);
+    expect(header).toBeInTheDocument();
+    expect(transactions).toBeInTheDocument();
+    expect(trade).toBeInTheDocument();
+  });
 
-test('renders headers', () => {
-  render(<App />);
-  const header = screen.getByText(/Stock Market/i);
-  const transactions = screen.getByText(/Transaction/i);
-  const trade = screen.getByText(/Trade/i);
-  expect(header).toBeInTheDocument();
-  expect(transactions).toBeInTheDocument();
-  expect(trade).toBeInTheDocument();
-});
-
-test('getDiferenceInMinutes', () => {
-  render(<App />);
-  const getDiferenceInMinutes = jest.fn();
-  const a= new Date();
-  const b= new Date();
-  expect(getDiferenceInMinutes).toBeCalledWith(b,a);
+  test('getDiferenceInMinutes', () => {
+    render(<App />);
+    const getDiferenceInMinutes = jest.fn();
+    const a= new Date();
+    const b= new Date();
+    expect(getDiferenceInMinutes).toBeCalledWith(b,a);
+  });
 });
